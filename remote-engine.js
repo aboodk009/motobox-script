815ad961ab38004e58943e8e1da2d3067200d77b
aboodk009 <abdalhalimkilany009@gmail.com>
2026-09-25 05:07:33 +0300

    Update remote-engine.js

--- remote-engine.js
+++ remote-engine.js
@@ -91,7 +91,7 @@ function showInPageToast(phone, name, fingerprint, duration) {
   
   const titleDiv = document.createElement("div");
   titleDiv.style.cssText = "font-size: 15px; font-weight: bold; margin-bottom: 4px; text-align: center;";
-titleDiv.textContent = `⚠️ تنبيه: تم اكتشاف طلب مكرر للعميل ${name} (${phone})`;  const subDiv = document.createElement("div");
+  titleDiv.textContent = `🔥 تجربة التحديث المباشر للعميل ${name} (${phone})`;
   subDiv.style.cssText = "font-size: 11px; opacity: 0.9; font-weight: normal; text-align: center;";
   subDiv.textContent = `اختبار التحديث السريع من GitHub!`;
 
